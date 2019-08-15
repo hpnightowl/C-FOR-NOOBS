b18447c5b457b6bdcade7d2c63ac3ecb3d12ff17
hpnightowl <iamhp2k@gmail.com>
2019-08-15 23:19:46 +0530

    Add Codes

---
+++ ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+        {
+            char ch=0;
+            for(int i=0 ;i<=128;i++)
+            {
+                printf("ASCII Character %c and Value = %d \n",ch,i);
+                ch++;
+             }
+                return 0;
+        }
