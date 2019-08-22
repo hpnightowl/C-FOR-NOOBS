6164cd520720df911fd882a2ddf7821fbe138e1c
HARSH <iamhp2k@gmail.com>
2019-08-22 15:44:00 +0530

    Added Codes

---
+++ Shifts.c
@@ -0,0 +1,12 @@
+a#include<stdio.h>
+
+void main()
+    {
+        int a;
+        printf("Enter a number: ");
+        scanf("%d",&a);
+        printf("\nOutput Of Right Shift Task: %d",a>>2); //dividing by 2...n here n=2
+        printf("\nOutput Of Right Shift Task: %d",a>>4); //dividing by 2...n here n=4 
+        printf("\nOutput Of Left Shift Task: %d",a<<2); //Multiplying by 2...n here n=2
+        printf("\nOutput Of Left Shift Task: %d",a<<4); //Multiplying by 2....n here n=4
+    }
