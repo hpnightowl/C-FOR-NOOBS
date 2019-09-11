fbf367c72b82f3a93ce156f3c5c7a1d13315ac31
HARSH <iamhp2k@gmail.com>
2019-09-11 20:58:55 +0530

    Added

---
+++ Assignments/compare2nos_q10.c
@@ -0,0 +1,16 @@
+/*
+``Question 10:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+
+void main()
+            {
+                int a,b;
+                scanf("%d %d",&a,&b);
+                (a>b)?printf("a is greater"):printf("b is greater");
+            }
