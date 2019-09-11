5ced1bde66bff06cf5f31bf2cc8b2e0e498e7f88
HARSH <iamhp2k@gmail.com>
2019-09-11 21:09:00 +0530

    Added

---
+++ Assignments/rectperi.c
@@ -0,0 +1,16 @@
+/*
+``Question 14:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+
+void main()
+            {
+                int a,b;
+                scanf("%d %d",&a,&b);
+                printf("Perimeter of Rectangle: %d",2*(a+b));
+            }
