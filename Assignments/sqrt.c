9ddadfa90e4ef9227d45f67e066fb328d52361e0
HARSH <iamhp2k@gmail.com>
2019-09-11 21:23:12 +0530

    Added

---
+++ Assignments/sqrt.c
@@ -0,0 +1,17 @@
+/*
+``Question 16:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+void main()
+            {
+                float a;
+                scanf("%f",&a);
+                printf("Square Root Of numbers: %f",pow(a,1/2.0));
+            }
