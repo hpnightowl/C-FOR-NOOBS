b9449eb27612e32d539d7a66eb6999f31e393401
HARSH <iamhp2k@gmail.com>
2019-09-11 21:03:47 +0530

    Added

---
+++ Assignments/oddandeven.c
@@ -0,0 +1,16 @@
+/*
+``Question 12:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+
+void main()
+            {
+                int a;
+                scanf("%d",&a);
+                (a%2==0)?printf("Even"):printf("Odd");
+            }
