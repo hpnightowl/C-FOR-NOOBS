1056526d006409309b335639cd01706a01000127
HARSH <iamhp2k@gmail.com>
2019-09-11 21:04:58 +0530

    Added

---
+++ Assignments/posandneg.c
@@ -0,0 +1,16 @@
+/*
+``Question 13:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+
+void main()
+            {
+                int a;
+                scanf("%d",&a);
+                (a>=0)?printf("Positive"):printf("Negative");
+            }
