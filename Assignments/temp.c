7a86cdc95cd41c0fe02b5a781b06da59f3019d88
HARSH <iamhp2k@gmail.com>
2019-09-11 21:21:00 +0530

    Added

---
+++ Assignments/temp.c
@@ -0,0 +1,16 @@
+/*
+``Question 15:
+                Harsh Pal
+                19BAI10127
+                CSE1001
+                F11+F12+F13
+*/
+
+#include<stdio.h>
+
+void main()
+            {
+                float T;
+                scanf("%f",&T);
+                printf("Coverted Temperature: %f",T*(9/5.0)+32);
+            }
