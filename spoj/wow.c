b5067a78b8c7fd37024186f1160c5c523b6cdba6
HARSH <iamhp2k@gmail.com>
2019-09-24 08:58:01 -0700

    Added Codes

---
+++ spoj/wow.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+            {
+                int n;
+                scanf("%d",&n);
+                printf("W");
+                for(int i=0;i<n;i++)
+                    printf("o");
+                printf("w");
+            }
